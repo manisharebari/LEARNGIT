aebec73d779e052a6040d5529c0719ed57088262
manisharebari <manisharebari24@gmail.com>
2025-02-25 00:07:53 +0530

    added msg in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum {
     public static void main(String[] args) {
         int a=5;
         int b=3;
-        System.err.println(a+b);
+        System.err.println("sum is:" +a+b);
     }
 }
